6416c1db894195a5942763f2202587157717f27b
Juan <ezio4213@gmail.com>
2025-01-22 11:34:29 -0600

    scroll v1

--- scroll.js
+++ scroll.js
@@ -1,64 +1,72 @@
+//este codigo si funciona su scroll, pero no tiene la nevagacion entre vistas.
+
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, ScrollView, StatusBar, SafeAreaView, TouchableOpacity, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
 import { useWindowDimensions } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import styles from './estilos/app_st';
-import ModalRegistro from './componentes/ModalRegistro';
-import AwesomeAlert from 'react-native-awesome-alerts';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import AgendarCitas from './paginas/citas';
+import ModalRegistro from './componentes/ModalRegistro'; // Asegúrate de importar correctamente el componente ModalRegistro
+import AwesomeAlert from 'react-native-awesome-alerts'; // Cambia la importación
 
-const Stack = createStackNavigator();
-
-// Componente HomeScreen separado
-function HomeScreen({ navigation }) {
+export default function App() {
   const { width, height } = useWindowDimensions();
-  const [showServices, setShowServices] = useState(true);
-  const [showLoginForm, setShowLoginForm] = useState(false);
-  const [isModalVisible, setModalVisible] = useState(false);
-  const [showAlert, setShowAlert] = useState(false);
-  const [alertServiceName, setAlertServiceName] = useState('');
-
-  const handleServiceClick = (screenName) => {
-    navigation.navigate(screenName);
-  };
+  const [showServices, setShowServices] = useState(true); // Estado de los servicios
+  const [showLoginForm, setShowLoginForm] = useState(false); // Estado del formulario de login
+  const [isModalVisible, setModalVisible] = useState(false); // Nuevo estado para controlar la visibilidad del modal
+  const [showAlert, setShowAlert] = useState(false); // Estado para controlar la visibilidad de la alerta
+  const [alertServiceName, setAlertServiceName] = useState(''); // Para almacenar el nombre del servicio
 
   const toggleServicesAndLogin = () => {
-    setShowServices(false);
-    setShowLoginForm(true);
+    setShowServices(false); // Oculta los servicios
+    setShowLoginForm(true); // Muestra el formulario de login
   };
 
   const toggleBackToServices = () => {
-    setShowServices(true);
-    setShowLoginForm(false);
+    setShowServices(true); // Muestra los servicios
+    setShowLoginForm(false); // Oculta el formulario de login
   };
 
   const handleServicesClick = () => {
-    setShowServices(true);
-    setShowLoginForm(false);
+    setShowServices(true); // Muestra los servicios
+    setShowLoginForm(false); // Asegúrate de ocultar el login si es visible
   };
 
   const handleAdminClick = () => {
-    setShowServices(false);
-    setShowLoginForm(true);
+    setShowServices(false); // Oculta los servicios
+    setShowLoginForm(true); // Muestra el formulario de login
+  };
+
+  // Función para mostrar el AwesomeAlert cuando se hace clic en un servicio
+  const handleServiceClick = (serviceName) => {
+    console.log('Botón presionado:', serviceName); // Verificar si se está disparando
+    setAlertServiceName(serviceName); // Almacenar el nombre del servicio
+    setShowAlert(true); // Mostrar la alerta
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar translucent backgroundColor="transparent" />
-      <LinearGradient
-        colors={['#333333', 'transparent']}
-        style={styles.header}
-      >
-        <Text style={styles.title}>Bienvenido a Nails ✨</Text>
-      </LinearGradient>
+      <StatusBar
+        translucent
+        backgroundColor="transparent"
+        // barStyle="light-content"
+      />
+      
 
-      <ScrollView 
-        style={styles.scrollView}
-        contentContainerStyle={styles.scrollViewContent}
+      <ScrollView
+        contentContainerStyle={styles.scrollView}
+        bounces={false}
       >
+        <LinearGradient
+          colors={['#333333', 'transparent']}
+          style={styles.header}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 0, y: 1 }}
+          align-items="center"
+        >
+          <Text style={styles.title}>Bienvenido a Nails ✨</Text>
+        </LinearGradient>
+
         <View style={styles.content}>
           <Text style={styles.text}>Esta es una Aplicacion en la que podras tener los servicios mas completos a tu alcance.</Text>
 
@@ -71,11 +79,17 @@ function HomeScreen({ navigation }) {
             <Text style={styles.transparentText}>Aqui encontraras nuestros principales canales para servicios o administracion.</Text>
 
             <View style={styles.buttonContainer}>
-              <TouchableOpacity style={styles.button} onPress={handleServicesClick}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={handleServicesClick} // Actualizado para mostrar servicios
+              >
                 <Icon name="design-services" size={20} color="#fff" />
                 <Text style={styles.buttonText}>Servicios</Text>
               </TouchableOpacity>
-              <TouchableOpacity style={styles.button} onPress={handleAdminClick}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={handleAdminClick} // Actualizado para mostrar administrador
+              >
                 <Icon name="admin-panel-settings" size={20} color="#fff" />
                 <Text style={styles.buttonText}>Administrador</Text>
               </TouchableOpacity>
@@ -99,11 +113,18 @@ function HomeScreen({ navigation }) {
                   <Text style={styles.buttonText}>Iniciar Sesión</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity style={styles.buttonBack} onPress={toggleBackToServices}>
+                <TouchableOpacity
+                  style={styles.buttonBack}
+                  onPress={toggleBackToServices}
+                >
                   <Text style={styles.buttonText}>Volver a los Servicios</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity style={styles.registerLinkContainer} onPress={() => setModalVisible(true)}>
+                {/* Enlace para abrir el modal de registro */}
+                <TouchableOpacity
+                  style={styles.registerLinkContainer}
+                  onPress={() => setModalVisible(true)} // Cambia el estado para mostrar el modal
+                >
                   <Text style={styles.registerLinkText}>Regístrate aquí</Text>
                 </TouchableOpacity>
               </KeyboardAvoidingView>
@@ -114,7 +135,7 @@ function HomeScreen({ navigation }) {
                 <Text style={styles.servicesTitle}>Nuestros Servicios</Text>
                 <TouchableOpacity
                   style={styles.serviceButton}
-                  onPress={() => handleServiceClick('AgendarCitas')}
+                  onPress={() => handleServiceClick('Agendar citas')}
                 >
                   <Icon name="calendar-today" size={20} color="#000" />
                   <Text style={styles.serviceText}>Agendar citas</Text>
@@ -139,15 +160,16 @@ function HomeScreen({ navigation }) {
         </View>
 
         <ModalRegistro visible={isModalVisible} onClose={() => setModalVisible(false)} />
-        </ScrollView>
+
         <LinearGradient
           colors={['transparent', '#333333']}
           style={styles.footer}
           start={{ x: 0, y: 0 }}
           end={{ x: 0, y: 1 }}
         />
-      
+      </ScrollView>
 
+      {/* Mostrar el alert usando AwesomeAlert */}
       <AwesomeAlert
         show={showAlert}
         title="Has Seleccionado:"
@@ -162,61 +184,5 @@ function HomeScreen({ navigation }) {
       />
     </SafeAreaView>
   );
-}
-
-// Pantallas adicionales
-function AgendarCitasScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <AgendarCitas />  
-    </View>
-  );
-}
-
-
-
-function CotizarScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Pantalla de Cotizar</Text>
-    </View>
-  );
-}
-
-function DiseñosScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Pantalla de Diseños</Text>
-    </View>
-  );
-}
-
-// Componente App principal
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen 
-          name="Home" 
-          component={HomeScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen 
-          name="AgendarCitas" 
-          component={AgendarCitasScreen}
-          options={{ title: 'Agendar Citas' }}
-        />
-        <Stack.Screen 
-          name="Cotizar" 
-          component={CotizarScreen}
-          options={{ title: 'Cotizar' }}
-        />
-        <Stack.Screen 
-          name="Diseños" 
-          component={DiseñosScreen}
-          options={{ title: 'Diseños' }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+   
 }
